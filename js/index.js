d2c0142b6873b487ae64101e2fc46fc15ca377af
Maya0035ac <maya0035@algonquinlive.com>
2025-05-13 10:50:18 -0400

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,37 @@
+// Task 1
+let a = 3
+let b = 5
+let c
+
+let output = `
+a = ${a};
+b = ${b};
+c = ${c};
+-----------
+a + b = ${a + b}
+a - b = ${a - b}
+a * b = ${a * b}
+a / b = ${a / b}
+a % b = ${a % b}
+a += b = ${a += b}
+a -= b = ${a -= b}
+a *= b = ${a *= b}
+a /= b = ${a /= b}
+a %= b = ${a %= b}
+a == b = ${a == b}
+a != b = ${a != b}
+a > b = ${a > b}
+a < b = ${a < b}
+!a && !c = ${!a && !c}
+!a || !c = ${!a || !c}`
+
+// Output to console
+console.log(output)
+
+// Task 2
+let first_name = "Mayank"
+let last_name = "Mayank"
+let email = "maya0035@algonquinlive.com"
+
+let result = `My name is ${first_name} ${last_name}. You can contact me at ${email}.`
+console.log(result)
